3ee77ea142170f98a12d9e30a1664b0a29443f51
tkmagesh <tkmagesh77@gmail.com>
2016-02-20 13:20:22 +0530

    Async Patterns - Using Callbacks, Events, Promises and Deferred Objects

---
+++ Day-03/01-AsyncPatterns/public/program.js
@@ -0,0 +1,159 @@
+//Revealing Module Pattern
+var patterns = (function(){
+
+    /* Sync */
+    function addSync(x,y){
+        console.log("[Provider] processing ", x , " and ", y);
+        var result = x + y;
+        console.log("[Provider] returning the result");
+        return result;
+    }
+
+    function addSyncClient(x,y){
+        console.log("[Consumer] trigger addSync");
+        var result = addSync(x,y);
+        console.log("[Consumer] result = ", result);
+    }
+
+    /* Async - callbacks */
+    function addAsyncUsingCallback(x,y, onResult){
+        console.log("[Provider] processing ", x , " and ", y);
+        setTimeout(function(){
+            var result = x + y;
+            console.log("[Provider] returning the result");
+            if (typeof onResult === 'function')
+                onResult(result);
+        },5000);
+    }
+
+    function addAsyncUsingCallbackClient(x,y){
+        console.log("[Consumer] trigger addAsyncUsingCallback");
+        addAsyncUsingCallback(x,y, function(result){
+            console.log("[Consumer] result = ", result);
+        });
+    }
+
+    /* Async - Events */
+    var addAsyncUsingEvents = (function(){
+        var onResultCallbacks = [];
+        function add(x,y){
+            console.log("[Provider] processing ", x , " and ", y);
+            setTimeout(function(){
+                var result = x + y;
+                console.log("[Provider] returning the result");
+                onResultCallbacks.forEach(function(callback){
+                    if (typeof callback === 'function')
+                        callback(result);
+                });
+            },5000);
+        }
+        function onResult(callback){
+            onResultCallbacks.push(callback);
+        }
+        return {
+            add : add,
+            onResult : onResult
+        };
+    })();
+
+    /*var addAsyncUsingPromise = function(x, y){
+
+        var promise = new Promise(function(resolve, reject){
+            console.log("[Provider] processing ", x , " and ", y);
+            setTimeout(function(){
+                var result = x + y;
+                console.log("[Provider] returning the result");
+                resolve(result);
+            },5000);
+        });
+
+        return promise;
+    };*/
+
+    var addAsyncUsingPromise = function(x, y){
+
+        var deferred = Promise.defer();
+        console.log("[Provider] processing ", x , " and ", y);
+        setTimeout(function(){
+            var result = x + y;
+            console.log("[Provider] returning the result");
+            deferred.resolve(result);
+        },5000);
+        return deferred.promise;
+    };
+
+    var addAsyncUsingCancellable = function(x, y){
+
+        var deferred = Promise.defer();
+        var cancelled = false;
+        console.log("[Provider] processing ", x , " and ", y);
+        var timer = setTimeout(function(){
+            var result = x + y;
+            console.log("[Provider] returning the result");
+            if (!cancelled)
+                deferred.resolve(result);
+        },5000);
+        var cancellablePromise = {
+           cancel : function(){
+               clearTimeout(timer);
+               cancelled = true;
+           },
+           promise : deferred.promise
+        };
+        return cancellablePromise;
+    };
+
+
+    return {
+        addSyncClient : addSyncClient,
+        addAsyncUsingCallbackClient : addAsyncUsingCallbackClient,
+        addAsyncUsingEvents : addAsyncUsingEvents,
+        addAsyncUsingPromise : addAsyncUsingPromise,
+        addAsyncUsingCancellable : addAsyncUsingCancellable
+    };
+})();
+
+/*function getBugs(){
+   var deferred = Promise.defer();
+   var myPromise = deferred.promise;
+
+   var fetchPromise = fetch("http://localhost:3000/bugs");
+   fetchPromise.then(function(response){
+     var p = response.json();
+     p.then(function(data){
+        deferred.resolve(data);
+     });
+   });
+   return myPromise;
+}*/
+
+/*
+function getBugs(){
+   var fetchPromise = fetch("http://localhost:3000/bugs")
+   var myPromise = fetchPromise.then(function(response){
+       var jsonPromise = response.json();
+       var dataPromise = jsonPromise.then(function(data){
+          return data;
+       });
+       return dataPromise;
+   });
+   return myPromise;
+ }
+*/
+
+function getBugs(){
+   return fetch("http://localhost:3000/bugs")
+    .then(function(response){
+       return response.json().then(function(data){
+          return data;
+       });
+   });
+ }
+
+
+function printBugs(){
+   var getBugsPromise = getBugs();
+   getBugsPromise.then(function(bugs){
+     console.table(bugs);
+   });
+}
